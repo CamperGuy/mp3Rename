af0e128747e68bf5865f595f6d3f512f8d30f84f
Joel Helbling <helblingjoel@gmail.com>
2020-05-12 00:14:27 +0100

    Expanded Tag Filter
    Added a whole bunch of entries and variations to trash

--- rename.py
+++ rename.py
@@ -12,10 +12,15 @@ for filename in os.listdir(path):
         ID = ID[-12:]
         editName = filename.replace(ID, '')
         editName = editName.replace(".mp3", '')
-        trash = ["(Official Video)", "(Official Audio)", "[Audio]", "(Audio)", "[Monstercat Release]", 
-                    "(Lyric Video)", "(lyrics)", "(Lyric Video)", "(official audio)",
-                    "(official video)", "[NCS Release]", "(Official Music Video)", 
-                    "[Official Lyric Video]", "Visualiser", "(Official Lyric Video)",
+        trash = [   "Official Music Video", "official music video", "(Official Music Video)", "(official music video)", "[Official Music Video]", "[official music video]",   
+                    "Official Video", "official video", "(Official Video)", "(official video)", "[Official Video]", "[official video]", 
+                    "Official Audio", "official audio", "(Official Audio)", "(official audio)", "[Official Audio]", "[official audio]",
+                    "Official Lyric Video", "official lyric video", "(Official Lyric Video)", "(official lyric video)", "[Official Lyric Video]", "[official lyric video]", "Official Lyrics", "official lyrics", "(Official Lyrics)", "(official lyrics)",
+                    "Lyric Video", "lyric video", "(Lyric Video)", "(lyric video)", "[Lyric Video]", "[lyric video]", "Lyrics", "lyrics", "(Lyrics)", "(lyrics)",
+                    "Audio", "audio", "(Audio)", "(audio)", "[Audio]", "[audio]",
+                    "Video", "video", "(Video)", "(video)", "[Video]", "[video]",
+                    "[Monstercat Release]",  "[NCS Release]",
+                   , "Visualiser",
                 ]
         for item in trash:
             if item in editName:
