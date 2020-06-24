6ea1059531689778ab4cf36dd42f424556100fda
Joel Helbling <helblingjoel@gmail.com>
2020-06-24 21:17:50 +0100

    Added setup.py
    This installer sets up a virtual environment and installs the dependency ffmpeg-python into that before it self-destructs.

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+import platform
+
+if platform.system() == "Windows":
+    print("Windows virtualenv setup and ffmpeg install")
+    os.system("python -m pip install --user virtualenv")
+    os.system("python -m venv mp3rename")
+    os.system(".\\mp3rename\\Scripts\\activate")
+    os.system("pip install ffmpeg-python")
+    os.system(".\\mp3rename\\Scripts\\deactivate")
+else:
+    print("Unix virtualenv setup and ffmpeg install")
+    os.system("python -m pip install --user virtualenv")
+    os.system("python -m venv mp3rename")
+    os.system("source /mp3rename/bin/activate")
+    os.system("pip install ffmpeg-python")
+    os.system("deactivate")
+
+os.system("python rename.py")
+os.system("remove setup.py")
